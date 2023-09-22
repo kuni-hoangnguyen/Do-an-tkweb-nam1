3932c030ecbcca9533ecc3d0eebfc4d97d8fa1e6
kuni <kuniday711@gmail.com>
2023-09-22 12:57:16 +0700

    up

---
+++ .history/JS/Login_20230922102636.js
@@ -0,0 +1,9 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm2").style.display = "none";
+}
+
---
+++ .history/JS/Login_20230922102649.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function openForm2() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm2").style.display = "none";
+}
+
